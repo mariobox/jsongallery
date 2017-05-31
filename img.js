6129fe6bde75b14ae2b06d02d29a29e4b29f2877
mariobox <mariosc@gmail.com>
2017-05-30 23:27:43 -0400

    fix js file

--- img.js
+++ img.js
@@ -4,13 +4,14 @@ $(document).ready(function() {
      // the AJAX part
     
     function displayPhotos(data) {
-      var photoHTML;
+      var photoHTML = "";
       $.each(data.items,function(i,photo) {
-        photoHTML += '<p><img src="' + photo.url + '></p><br />' + '<span>' + photo.description + '</span>';
+        photoHTML += '<p><img src="' + photo.url + '"></p><br />' + '<span>' + photo.description + '</span>';
       }); // end each
       
       $('#photos').html(photoHTML);
-    }
+    };
+
     $.getJSON("mscphotos.json", displayPhotos);
     
     
