443580164e2714f053123bc44045206c801647c0
mariobox <mariosc@gmail.com>
2017-05-30 23:03:45 -0400

    initial commit

---
+++ img.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+     // the AJAX part
+    
+    function displayPhotos(data) {
+      var photoHTML;
+      $.each(data.items,function(i,photo) {
+        photoHTML += '<p><img src="' + photo.url + '></p><br />' + '<span>' + photo.description + '</span>';
+      }); // end each
+      
+      $('#photos').html(photoHTML);
+    }
+    $.getJSON(mscphotos.json, displayPhotos);
+    
+    
+
+  
+
+}); // end ready
